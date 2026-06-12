9100cd578edf55212a8e209312173e7b15205f68
Prachi Thakre <prachithakre253@gmail.com>
2026-06-12 20:13:30 +0530

    Updated project files

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-def addition(a,b):
-    return a + b
+def addition(a,b,c):
+    return a + b + c
 
-def minus(a,b):
-    return a - b
+def minus(a,b,d):
+    return a - b - d
 
-
+def div(a,b,c):
+    return a / b
